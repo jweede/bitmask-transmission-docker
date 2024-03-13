50afc9fbd279edfb8b7056d213a49d032fc7a289
Jon Wedaman <jweede@gmail.com>
2024-03-13 19:20:41 -0400

    cleanup

--- docker_bitmask/entrypoint.sh
+++ docker_bitmask/entrypoint.sh
@@ -1,76 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+here="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+
 function log {
     echo "$@" >&2
-}
-function check_dns {
-  python3 -c "import socket;socket.gethostbyname('apple.com')"
-}
-
-function fix_dns {
-#  python3 /root/resolve_spotty_dns.py "api.calyx.net" "calyx.net" "ifconfig.me"
-  log "Fixing DNS"
-  if ! check_dns; then
-    echo "nameserver 8.8.8.8" > /etc/resolv.conf
-    sleep 2
-  fi
-  check_dns
-}
-
-
-function template_vpn {
-  cat - >/etc/riseup-vpn.yaml <<YAML
----
-# /etc/riseup-vpn.yaml
-
-server: vpn12-nyc.riseup.net
-protocol: udp
-port: 53
-
-# excluded_routes: list servcies that should not be routed over VPN
-# can be an ipaddress, network or hostname
-# your local subnet is excluded by default
-excluded_routes:
-  - 8.8.8.8
-
-# os user/group
-user: root
-group: root
-YAML
-
-  mkdir -p /etc/openvpn/client
-  log "Templating openvpn config"
-  riseup-vpn-configurator --update
-  riseup-vpn-configurator -g
-}
-
-function start_vpn {
-    log "Starting bitmask"
-    openvpn --config /etc/openvpn/client/riseup.conf
-    openvpn_pid=$!
-    echo "OpenVPN launched as ${openvpn_pid}"
-}
-
-function setup_firewall {
-  set -euo pipefail
-  export DEBUG=true
-  local gateway
-  gateway="$(awk '$1 == "remote" { print $2; exit; }' /root/bitmask_ovpns/*.ovpn )"
-  [[ -n "${gateway}" ]]
-  python3 /root/bitmask-root firewall start "${gateway}"
 }
 
 export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 
 if [[ "${1:-}" == "" ]]; then
-#    fix_dns
-    template_vpn
+    log "Templating VPN"
+    "${here}"/template_vpn.sh
+    log "Starting bitmask"
     exec openvpn --config /etc/openvpn/client/riseup.conf
-#    start_vpn
-#    setup_firewall
-#    fix_dns
-    wait $openvpn_pid
 else
     exec "$@"
 fi
---
+++ docker_bitmask/template_vpn.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cat - >/etc/riseup-vpn.yaml <<YAML
+---
+# /etc/riseup-vpn.yaml
+
+server: vpn12-nyc.riseup.net
+protocol: udp
+port: 53
+
+# excluded_routes: list services that should not be routed over VPN
+# can be an ipaddress, network or hostname
+# your local subnet is excluded by default
+excluded_routes:
+  - 8.8.8.8
+
+# os user/group
+user: root
+group: root
+YAML
+
+mkdir -p /etc/openvpn/client
+
+riseup-vpn-configurator --update
+riseup-vpn-configurator -g
